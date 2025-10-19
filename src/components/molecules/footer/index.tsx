3edf402c8dbc688f2f4cee458d2e73bd30c09cd7
ocarballido <ocarballido@gmail.com>
2025-10-19 12:38:05 +0200

    footer updates

--- src/components/molecules/footer/index.tsx
+++ src/components/molecules/footer/index.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 
 const OcFooter = () => {
 	return (
-		<footer className="py-6 sm:py-4 px-4 mx-2 md:mx-3 bg-(--background-light) rounded-2xl mt-0 dark:text-[#a0b8e3]">
+		<footer className="py-6 sm:py-4 px-4 mx-2 md:mx-3 bg-(--background-light) rounded-2xl mt-0 text-gray-500 dark:text-[#a0b8e3]/70">
 			<div className="max-w-7xl md:w-full lg:w-4xl xl:w-full mx-auto text-sm font-medium flex flex-col sm:flex-row items-center gap-4 justify-between">
 				<div className="flex flex-col sm:flex-row items-center gap-3">
 					<Link
@@ -26,15 +26,23 @@ const OcFooter = () => {
 							className="block dark:hidden"
 							style={{ width: '24', height: 'auto' }}
 						/>
-						<p className="text-left opacity-50 grow-0 w-auto">
+						<p className="text-left grow-0 w-auto">
 							Oscar Carballido | UI Developer
 						</p>
 					</Link>
-					<div className="flex gap-3 items-center py-2 px-3 bg-neutral-950/10 dark:bg-[#293b54] dark:opacity-100 opacity-60 rounded-full">
+					<div className="flex gap-3 items-center py-2 px-3 bg-primary-10 dark:bg-[#293b54] rounded-full">
 						<Image
 							src="/static/footer/nextjs.svg"
 							alt="NextJS logo"
-							className=""
+							className="opacity-40 block dark:hidden"
+							width={50}
+							height={50}
+							style={{ width: '50', height: 'auto' }}
+						/>
+						<Image
+							src="/static/footer/nextjs-dark.svg"
+							alt="NextJS logo"
+							className="hidden dark:block opacity-70"
 							width={50}
 							height={50}
 							style={{ width: '50', height: 'auto' }}
@@ -45,12 +53,21 @@ const OcFooter = () => {
 							alt="NextJS logo"
 							width={60}
 							height={50}
+							className="opacity-40 block dark:hidden"
+							style={{ width: '60', height: 'auto' }}
+						/>
+						<Image
+							src="/static/footer/hygraph-dark.svg"
+							alt="NextJS logo"
+							width={60}
+							height={50}
+							className="opacity-70 hidden dark:block"
 							style={{ width: '60', height: 'auto' }}
 						/>
 					</div>
 				</div>
 				<nav>
-					<ul className="inline-flex gap-3 opacity-50">
+					<ul className="inline-flex gap-3">
 						<li>
 							<Link
 								href="https://github.com/ocarballido"
@@ -59,11 +76,11 @@ const OcFooter = () => {
 								scroll={false}
 							>
 								<Image
-									src="/static/icons/github-black.svg"
+									src="/static/icons/github-dark.svg"
 									height={16}
 									width={16}
 									alt="Github icon"
-									className="hidden dark:block"
+									className="hidden dark:block opacity-70"
 									style={{ width: '16', height: 'auto' }}
 								/>
 								<Image
@@ -71,7 +88,7 @@ const OcFooter = () => {
 									height={16}
 									width={16}
 									alt="Github icon"
-									className="block dark:hidden"
+									className="block dark:hidden opacity-40"
 									style={{ width: '16', height: 'auto' }}
 								/>
 								GitHub
@@ -85,10 +102,19 @@ const OcFooter = () => {
 								scroll={false}
 							>
 								<Image
+									src="/static/icons/linkedin-dark.svg"
+									height={16}
+									width={16}
+									alt="Github icon"
+									className="hidden dark:block opacity-70"
+									style={{ width: '16', height: 'auto' }}
+								/>
+								<Image
 									src="/static/icons/linkedin-black.svg"
 									height={16}
 									width={16}
 									alt="Github icon"
+									className="block dark:hidden opacity-40"
 									style={{ width: '16', height: 'auto' }}
 								/>
 								LinkedIn
