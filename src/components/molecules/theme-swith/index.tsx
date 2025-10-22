6a2d90b5cc8c336b7284ab3a5945ee44c87952da
ocarballido <ocarballido@gmail.com>
2025-10-22 14:48:51 +0200

    rotation mode switch

--- src/components/molecules/theme-swith/index.tsx
+++ src/components/molecules/theme-swith/index.tsx
@@ -20,22 +20,30 @@ export default function ThemeSwitch() {
 			</div>
 		);
 
+	const handleToggle = () => {
+		setTheme(resolvedTheme === 'light' ? 'dark' : 'light');
+	};
+
 	return (
 		<div
-			className="rounded-full p-2 bg-primary-10 hover:bg-primary-50/60 focus:bg-primary-50 dark:bg-[#293B54] dark:hover:bg-[#354a68] dark:focus:bg-[#354a68] ml-2 w-[40px] h-[40px] hover:cursor-pointer overflow-hidden transition-colors"
-			onClick={() =>
-				setTheme(resolvedTheme === 'light' ? 'dark' : 'light')
-			}
+			className="relative rounded-full p-2 bg-primary-10 hover:bg-primary-50/60 focus:bg-primary-50 dark:bg-[#293B54] dark:hover:bg-[#354a68] dark:focus:bg-[#354a68] ml-2 w-[40px] h-[40px] hover:cursor-pointer transition-colors overflow-hidden"
+			onClick={handleToggle}
 		>
 			<div
-				className={`w-[56px] h-[24px] flex gap-[8px] transition-all ${
-					resolvedTheme === 'light'
-						? 'translate-x-0'
-						: '-translate-x-8'
-				}`}
+				className="absolute top-[7px] -left-[15px] w-[70px] h-[70px] transform transition-transform duration-500 ease-in-out"
+				style={{
+					rotate: resolvedTheme === 'dark' ? '0deg' : '-90deg',
+				}}
 			>
-				<OcIconLightMode />
-				<OcIconDarkMode />
+				<div className="absolute inset-0 flex flex-col gap-[20px] justify-center items-center">
+					<OcIconDarkMode />
+					<span className="w-[24px] h-[24px]"></span>
+				</div>
+
+				<div className="absolute inset-0 rotate-90 flex flex-col gap-[20px] justify-center items-center">
+					<OcIconLightMode />
+					<span className="w-[24px] h-[24px]"></span>
+				</div>
 			</div>
 		</div>
 	);
