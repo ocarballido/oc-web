8299a8cff5465aa93170538381a424c9a8a529c4
ocarballido <ocarballido@gmail.com>
2025-10-24 14:07:20 +0200

    better animation, and warnings removed

--- src/components/atoms/logo/index.tsx
+++ src/components/atoms/logo/index.tsx
@@ -8,18 +8,7 @@ const OcLogo = () => {
 				alt="Oscarballido logo"
 				width={100}
 				height={72}
-				priority
-				src="/static/appbar/ocarballido-logo-dark.svg"
-				className="hidden dark:block opacity-60"
-				style={{ width: '100px', height: 'auto' }}
-			/>
-			<Image
-				alt="Oscarballido logo"
-				width={100}
-				height={72}
-				priority
 				src="/static/appbar/ocarballido-logo.svg"
-				className="block dark:hidden"
 				style={{ width: '100px', height: 'auto' }}
 			/>
 		</Link>
